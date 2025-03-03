f86ff875aa015a178e716239b7c6a8fe6dfe716c
Walker <bilalwalker687@gmail.com>
2025-03-03 19:34:19 +0500

    create migrations for Pagevisits

--- src/visits/models.py
+++ src/visits/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class PageVisit(models.Model):
+    # database table
+    path = models.TextField(blank=True, null=True) # column
+    timestamp = models.DateTimeField(auto_now_add=True) # column
