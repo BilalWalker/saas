4cf0b76e19871349d382706ecde28319858451e1
Walker <bilalwalker687@gmail.com>
2025-03-03 21:06:25 +0500

    add migrations and some other stuff

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -1,4 +1,17 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from visits.models import PageVisit
 
 def home_page_view(request, *args, **kwargs):
-    return HttpResponse("<h1> Hello World</h1>")
+    qs = PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path=request.path)
+    my_title = "my Pageee"
+    path = request.path
+    PageVisit.objects.create(path=request.path)
+    my_context = {
+        'page_title': my_title,
+        'page_visit_count': page_qs.count(),
+        'total_visit_count': qs.count(),
+    }
+    return render(request, 'home.html', my_context)
