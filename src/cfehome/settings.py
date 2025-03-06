f8ecf8a76dbc3ee1115935b26cb5c8fbfe90a61b
Walker <bilalwalker687@gmail.com>
2025-03-06 18:56:20 +0500

    add database url and dependencies in requirements.txt file

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+#import os
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+mekkh=&@gx+j)n@%wo!!nz)2qz)oxab=z16%p+8%x5qoqivo+'
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = str(os.environ.get("DEBUG")).lower() == "true"
+DEBUG = config("DJANGO_DEBUG", cast=bool)
+
+print("DEBUG", DEBUG)
 
 ALLOWED_HOSTS = [
     ".railway.app"
