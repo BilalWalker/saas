073fbe6df6dd44611bf259f0835426a9180c3db6
Walker <bilalwalker687@gmail.com>
2025-03-09 21:24:37 +0500

    login template and logic

--- src/auth/views.py
+++ src/auth/views.py
@@ -4,13 +4,16 @@ from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def login_view(request):
-    username = 'cfe2'#request.POST["username"]
-    password = 'bilalkhan123'#request.POST["password"]
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-        print("login here!")
-        return redirect("/")
+    #print(request.method, request.POST or None)
+    if request.method == "POST":
+        username = request.POST.get("username") or None
+        password = request.POST.get("password") or None
+        if all([username, password]):
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                print("login here!")
+                return redirect("/")
     return render(request, "auth/login.html", {})
 
 # def register_view(request):
