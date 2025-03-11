54843f4878acb1ca109f4c14375c9d54508ccb81
Walker <bilalwalker687@gmail.com>
2025-03-11 19:24:44 +0500

    change navbar for better navigation

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("login/", auth_views.login_view),
     path("register/", auth_views.register_view),
-    path('', home_view),
+    path('', home_view, name='home'),
     path('about/', about_view),
     path('accounts/', include('allauth.urls')),
 ]
