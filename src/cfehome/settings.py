d29b91372fbb20c27c2a5910d72bc426ca3716b6
Walker <bilalwalker687@gmail.com>
2025-03-13 20:06:33 +0500

    user permissions

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -70,6 +70,7 @@ INSTALLED_APPS = [
     # my apps
     'visits',
     "commando",
+    "profiles",
     # third-party-apps
     'allauth_ui',    
     'allauth',
---
+++ src/profiles/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+@login_required
+def profile_view(request, username=None, *args, **kwargs):
+    user = request.user
+    # <app_label>.view_<model_name>
+    # <app_label>.add_<model_name>
+    # <app_label>.change_<model_name>
+    # <app_label>.delete_<model_name>
+    print('user.has_perm("auth.view_user")', user.has_perm("auth.view_user"))
+    print('user.has_perm("auth.view_user")', user.has_perm("visits.view_pagevisit"))
+
+    # profile_user_obj = User.objects.get(username=username)
+    profile_user_obj = get_object_or_404(User, username=username)
+    is_me = profile_user_obj == user
+    if is_me:
+        if user.has_perm("visits.view_pagevisit"):
+            pass
+            # qs = PageVisit.objects.all()
+    return HttpResponse(f"Hello there {username} = {profile_user_obj.id} - {user.id} {is_me}")
