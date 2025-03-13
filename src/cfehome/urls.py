f348b57eb26b37aa92a85a92d821d8a82d612682
Walker <bilalwalker687@gmail.com>
2025-03-13 18:44:59 +0500

    staff member required functionality

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -22,16 +22,18 @@ from .views import (
     home_view, 
     about_view, 
     pw_protected_view, 
-    user_only_view
+    user_only_view,
+    staff_only_view,
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', home_view, name='home'),
     path("login/", auth_views.login_view),
     path("register/", auth_views.register_view),
-    path('', home_view, name='home'),
     path('about/', about_view),
     path('accounts/', include('allauth.urls')),
     path('protected/', pw_protected_view),
-    path('protected/user-only', user_only_view),
+    path('protected/user-only/', user_only_view),
+    path('protected/staff-only/', staff_only_view),
+    path('admin/', admin.site.urls),
 ]
