24887d0ddb2cdfb191830c1140dd52be877e717c
Walker <bilalwalker687@gmail.com>
2025-03-15 21:37:45 +0500

    profiles

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -7,20 +7,20 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 @login_required
-def profile_view(request, username=None, *args, **kwargs):
-    user = request.user
-    # <app_label>.view_<model_name>
-    # <app_label>.add_<model_name>
-    # <app_label>.change_<model_name>
-    # <app_label>.delete_<model_name>
-    print('user.has_perm("auth.view_user")', user.has_perm("auth.view_user"))
-    print('user.has_perm("auth.view_user")', user.has_perm("visits.view_pagevisit"))
+def profile_list_view(request):
+    context = {
+        "object_list": User.objects.filter(is_active=True) # see all users that are active
+    }
+    return render(request, "profiles/list.html", context)
 
-    # profile_user_obj = User.objects.get(username=username)
+@login_required
+def profile_detail_view(request, username=None, *args, **kwargs):
+    user = request.user
     profile_user_obj = get_object_or_404(User, username=username)
     is_me = profile_user_obj == user
-    if is_me:
-        if user.has_perm("visits.view_pagevisit"):
-            pass
-            # qs = PageVisit.objects.all()
-    return HttpResponse(f"Hello there {username} = {profile_user_obj.id} - {user.id} {is_me}")
+    context = {
+        "object": profile_user_obj,
+        "instance": profile_user_obj,
+        "owner": is_me,
+    }
+    return render(request, "profiles/detail.html", context)
