3fe8c19bc5b14eb3f549c5d04f2f638a384800e1
Walker <bilalwalker687@gmail.com>
2025-04-11 19:27:52 +0500

    user subscription view and refresh

--- src/subscriptions/views.py
+++ src/subscriptions/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
-from subscriptions.models import SubscriptionPrice
+from subscriptions.models import SubscriptionPrice, UserSubscription
+import helpers.billing
+
+@login_required
+def user_subscription_view(request):
+    user_sub_obj, created = UserSubscription.objects.get_or_create(user=request.user)
+    # sub_data = user_sub_obj.serialize()
+    if request.method == "POST":
+        print("refresh sub")
+        if user_sub_obj.stripe_id:
+            sub_data = helpers.billing.get_subscription(user_sub_obj.stripe_id, raw=False)
+            for k, v in sub_data.items():
+                setattr(user_sub_obj, k, v)
+            user_sub_obj.save()
+        return redirect(user_sub_obj.get_absolute_url())
+    return render(request, 'subscriptions/user_detail_view.html', {"subscription": user_sub_obj})
+
 
 # Create your views here.
 def subscription_price_view(request, interval="month"):
